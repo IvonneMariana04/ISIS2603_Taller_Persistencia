caa45ee0a04f2613bdc5090ee51fa10449656395
Ivonne Mariana Cuncanchun Bernal <i.cuncanchunb@uniandes.edu.co>
2026-02-18 08:46:31 -0500

    Commit

---
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/services/PeliculaService.java
@@ -0,0 +1,45 @@
+package co.edu.uniandes.dse.TallerPersistencia.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.TallerPersistencia.entities.PeliculaEntity;
+import co.edu.uniandes.dse.TallerPersistencia.repositories.PeliculaRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class PeliculaService {
+    
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+
+    private void validarTituloNoVacio(PeliculaEntity peliculaEntity){
+        if (peliculaEntity.getTitulo().trim().isEmpty()){
+            throw new IllegalStateException("El titulo no puede ser vacio");
+        }
+    }
+
+    private void validarTituloNoRepetido(PeliculaEntity peliculaEntity){
+        if (peliculaRepository.existsByTitulo(peliculaEntity.getTitulo().trim())){
+            throw new IllegalStateException("Ya existe una pelicula con ese titulo");
+        }
+    }
+
+    private void validarAnioLanzamiento(PeliculaEntity peliculaEntity){
+        if (peliculaEntity.getAnioLanzamiento() > 1930){
+            throw new IllegalStateException("No puede ser anterior a 1930");
+        }
+    }
+
+    @Transactional
+    public void crearPelicula(PeliculaEntity peliculaEntity){
+        
+        validarTituloNoVacio(peliculaEntity);
+        validarTituloNoRepetido(peliculaEntity);
+        validarAnioLanzamiento(peliculaEntity);
+
+        peliculaRepository.save(peliculaEntity);
+    }
+
+}
---
+++ src/test/PeliculaServiceTest.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.edu.uniandes.dse.TallerPersistencia.entities.PeliculaEntity;
+import co.edu.uniandes.dse.TallerPersistencia.services.PeliculaService;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
+@Import(PeliculaService.class)
+
+
+public class PeliculaServiceTest {
+    
+    @Autowired
+    private PeliculaService peliculaService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private PodamFactory factory = new PodamFactoryImpl();
+
+    @Test
+    void testCrearPelicula(){
+        PeliculaEntity newEntity = factory.manufacturePojo(PeliculaEntity.class);
+        PeliculaEntity result = peliculaService.crearPelicula(newEntity);
+
+        PeliculaEntity entity = entityManager.find(PeliculaEntity.class, result.getId());
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+
+        assertEquals(newEntity, result);
+        assertEquals(newEntity.getTitulo(), entity.getTitulo);
+        assertEquals(newEntity.getAnioLanzamiento, entity.getAnioLanzamiento);
+        
+    }
+
+    @Test
+    void testAnioLanzamientoIncorrecto(){
+        PeliculaEntity newEntity = factory.manufacturePojo(PeliculaEntity.class);
+        
+        newEntity.setAnioLanzamiento(1900);
+
+        assertThrows(IllegalStateException.class, () -> {peliculaService.crearPelicula(newEntity);
+        });
+    }
+}
